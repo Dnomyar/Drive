448659fab4f419d744f49753b4f8ad6f38b2182c
Damien RAYMOND <dam.ray49@gmail.com>
2017-05-16 21:03:57 +0200

    Print output WIP

--- src/main/scala/fr/damienraymond/drive/Simulator.scala
+++ src/main/scala/fr/damienraymond/drive/Simulator.scala
@@ -10,46 +10,72 @@ class Simulator {
 
   private val dataFetcher: DataFetcher = new CSVDataFetcher
 
+
   def simulate(): Unit = {
 
     val data = dataFetcher.fetch()
 
     val bandFactor: Float = 1 // TODO
 
-    val output =
+    val output: Seq[(Int, List[AgentBreed])] =
       (0 until 15).map { year =>
-        data
-          .flatMap(handleOneDataRow(_, bandFactor))
-          .map((year, _))
+        (
+          year,
+          data.flatMap(handleOneDataRow(_, bandFactor))
+        )
       }
 
 
-    println(output.mkString("\n"))
+    //println(output.mkString("\n"))
+
+    printResult(output)
 
   }
 
 
   // TODO refactor
-  def handleOneDataRow(data: Agent, bandFactor: Float): Option[AgentBreed] = {
+  def handleOneDataRow(agent: Agent, bandFactor: Float): Option[AgentBreed] = {
 
     // TODO age incrementation
 
-    if (data.autoRenew){
+    if (agent.autoRenew){
       // Do nothing, maintain Breed<
-      Some(data.agentBreed)
+      Some(agent.agentBreed)
     }else{
       val rand = Math.random() * 3
-      val affinity = data.paymentAtPurchase / data.attributePrice + (rand * data.attributePromotions * data.inertiaForSwitch)
-      if(data.agentBreed == BreedC && affinity < (data.socialGrade * data.attributeBrand)){
+      val affinity = agent.paymentAtPurchase / agent.attributePrice + (rand * agent.attributePromotions * agent.inertiaForSwitch)
+      if(agent.agentBreed == BreedC && affinity < (agent.socialGrade * agent.attributeBrand)){
         // Switch Breed to Breed_NC
         Some(BreedNC)
-      } else if (data.agentBreed == BreedC && affinity < (data.socialGrade * data.attributeBrand * bandFactor)){
+      } else if (agent.agentBreed == BreedC && affinity < (agent.socialGrade * agent.attributeBrand * bandFactor)){
         // Switch Breed to Breed_C
         Some(BreedC)
       } else {
         None
       }
     }
+  }
+
+
+
+  def printResult(output: Seq[(Int, List[AgentBreed])]): Unit = {
+
+
+
+    output.foreach(e => printOneYear(e._1, e._2))
+  }
+
+
+  def printOneYear(year: Int, data: List[AgentBreed]): Unit = {
+
+    val nbBreadCAgents = data.count(_ == BreedC)
+    val nbBreadNCAgents = data.count(_ == BreedNC)
+
+    println()
+    println(s"Year ${year + 1}")
+    println(s"# Bread_C Agents : $nbBreadCAgents")
+    println(s"# Bread_NC Agents : $nbBreadNCAgents")
+
   }
 
 }
